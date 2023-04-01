d6818661334fa38d16bd45985a09366f63aff5f5
Maryam Fakhraei <M.Fakhraeisari@gmail.com>
2023-04-01 23:01:05 +0330

    Add Display Function And Fix Interface With Users

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from odd_order import fillOddOrder
 from doubly_even_order import fillDoublyEvenOrder
+from termcolor import *
+from time import *
 
 def fillMatrix(n):
     # Edge cases:
@@ -20,7 +22,32 @@ def fillMatrix(n):
     
     return magicSquare
 
+# Print the Magic Square
+def display(magic_square, n):
+    print(colored("Magic Square for %s order" %(n), "light_cyan"))
+    print(colored("------------------------", "light_cyan"))
+    for i in range(0, n):
+        for j in range(0, n):
+            print(colored('%3d ' %(magic_square[i][j]), "light_cyan"), end=' ')
+            # Go next row if print all column
+            if j == n - 1:
+                print()
+
+
 if __name__ == "__main__":
+    # Communication with the user
+    print(colored("===> Welcome to Magic Square Solver <=== \n", "light_yellow", attrs=["bold"]))
+
     # Get size from user
     size = int(input("Enter the Magic Square size : "))
-    magic_squar = fillMatrix(size)
+
+    # Run until user enter 0
+    while size != 0:
+        sleep(1)
+        magic_squar = fillMatrix(size)
+        display(magic_squar, size)
+        sleep(1)
+        size = int(input("\nEnter the Magic Square size (enter 0 if you want to exit) : "))
+    
+    # Developers
+    print(colored("\nThank you for choosing us :)\n>>>Developed by Maryam Fakhraei and Amirhossein Naseri<<<\n", "light_magenta"))
